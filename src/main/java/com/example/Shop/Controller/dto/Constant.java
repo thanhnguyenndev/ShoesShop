79766f32ef7179ccada09df6cb06fe2db1b13653
cuncunham1 <101930255+cuncunham1@users.noreply.github.com>
2023-05-14 15:32:17 +0700

    commit5

--- src/main/java/com/example/Shop/Controller/dto/Constant.java
+++ src/main/java/com/example/Shop/Controller/dto/Constant.java
@@ -1,7 +1,7 @@
 package com.example.Shop.Controller.dto;
 
 public interface Constant {
-	public String UPLOAD_FILE_ROOT = "C:/Users/admin/Desktop/ShoesShop/upload/";
+	public String UPLOAD_FILE_ROOT = "ShoesShop/upload/";
 	// nhớ đổi đường dẫn file upload
 	
 }
